b81006e1cf14d97712ba39392d0c5ac3a95d8ba5
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-11 13:47:28 -0800

    Basic Operators set next up logic w/ if ands no buts

--- alphaStat.py
+++ alphaStat.py
@@ -1,5 +1,3 @@
-First = 'Hello World'
-print(First)
 #Project Goals
 #Part 1 The Basics, I hope!
 #Make a program that can get the mean median and mode of a column or row
---
+++ basicPython.py
@@ -0,0 +1,29 @@
+#I have't used python mucb before so I'm moving my notes to this file!
+First = 'Hello World'
+#print(First)
+#different variable types
+int = 2010
+#print(int )
+string = 'bird is the word'
+#print(string )
+boollean = False
+#print(boollean )
+list = ['Luigi','Mario']
+#print(list )
+#to print an integer and a string together int needs to be converted to string
+print (string + " " + str(int))
+# same but reversed if you have a string with a number use int()
+
+#BASIC OPERATORS
+a = 5
+b = 4
+add = a + b
+sub = a - b
+mult = a * b
+div = a / b
+exp = a**b
+print ("Basic Operators " + str(add) +" "+ str(sub) +" "+  str(mult) +" "+  str(div) +" "+  str(exp))
+
+#LOGIC
+if a > b:
+    print(str(a) + " is greater than " + str(b))
