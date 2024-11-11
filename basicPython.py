9d03bc22b6412efb061800d0fca8ce1a19371416
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-11 14:48:04 -0800

    While Loops and Try Catching by a different name!

--- basicPython.py
+++ basicPython.py
@@ -2,7 +2,7 @@
 First = 'Hello World'
 #print(First)
 #different variable types
-int = 2010
+integer = 2010
 #print(int )
 string = 'bird is the word'
 #print(string )
@@ -29,10 +29,47 @@ if a > b:
     print(str(a) + " is greater than " + str(b))
 else: 
     print(str(b) + " is greater than " + str(a))
-
+#loops and other things that use: care indentation sensitve!
 #if can also be used to check if a bool is true.
 
-    #FOR LOOP
+#FOR LOOP
 for i in range(3):
     print('recursion', i + 1)
 print(range(3))
+#range by itself could be nice for alphastat
+
+#WHILE LOOP
+loop = 0
+while loop < 5:
+    loop = loop + 1
+    print(loop)
+while True:
+    #infinite loop
+    user_input = input('Enter 0 to end!')
+    if user_input == '0':
+        print('0 entered!')
+        break
+    else:
+        print('that aint 0')
+
+#FUNCTIONS
+#def is the keyword
+def hello_name(name):
+    print('Hey there ', name)
+
+#We calling functions now!
+hello_name('Username1')
+hello_name('Username2')
+
+#KEYWORD pass  allows for empty functions that are being worked on to not break the program while running
+def gaming():
+    pass
+#Works exactly like id expect from Lua
+
+#TRY & ACCEPT block
+num = input("Input a number: ")
+try:
+    print(10 + int(num))
+except:
+    print('Invalid Number')
+#Note python lets you name a variable 'int' which causes int to fail to convert strings to int
