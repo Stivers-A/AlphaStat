fd8ca57bd2e216d5b1a7fd2ac07b0a4026bb175b
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-16 10:40:41 -0800

    apple_quality was set so the average was 0, kinda hard to test results when half of the results are 0

--- basicCsv.py
+++ basicCsv.py
@@ -6,10 +6,10 @@ with open('apple_quality_unedited.csv','r') as csv_file:
     print(csv_reader)
     #all that shows up is 'csv.reader object at (location)
 
-    with open('new_vals.csv','w') as new_file:
-        csv_writer = csv.writer(new_file, delimiter='-')
-        for line in csv_reader:
-            print(line)
+    #with open('new_vals.csv','w') as new_file:
+       # csv_writer = csv.writer(new_file, delimiter='-')
+       # for line in csv_reader:
+        #    print(line)
             #csv_writer.writerow(line) commented out to prevent spam creation of new files
             #opens a file and and uses csv.reader, then uses csv.writer to copy the content of the original with the delimenter as a dash
 
@@ -18,3 +18,4 @@ with open('apple_quality_unedited.csv','r') as csv_file:
         #this renders each line of the csv as a line in the terminal 
         #print(line[2]) 
         # would print the second line
+    
