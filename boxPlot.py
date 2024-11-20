be93f42f703b6a9ffd2262f45e651a2331f86885
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-19 21:15:10 -0800

    started next python stat project

---
+++ boxPlot.py
@@ -0,0 +1,10 @@
+#TODO, allow user to select file then select row or column(s) then render box plot of columns 
+
+#sample boxplot
+#import pandas as pd
+
+#data = {'values': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+#df = pd.DataFrame(data)
+
+#df.boxplot(column='values')
+#plt.show()
