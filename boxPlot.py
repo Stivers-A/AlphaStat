1885e9e8d3afa2dff7215a8ac0cf5db5b46ce273
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-20 20:07:11 -0800

    Got a good looking boxplot now for the rest of the program

--- boxPlot.py
+++ boxPlot.py
@@ -1,4 +1,6 @@
-#TODO, allow user to select file then select row or column(s) then render box plot of columns 
+#TODO GUI
+#TODO allow user to select file then select row or column(s) 
+#TODO then render box plot of columns 
 
 #sample boxplot
 #import pandas as pd
@@ -8,3 +10,18 @@
 
 #df.boxplot(column='values')
 #plt.show()
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+selection = []
+with open ('Real_Estate_Sales_2001-2022_GL.csv','r',newline='') as csvfile1:
+    reader = csv.DictReader(csvfile1)
+    for row in reader:
+        if row != 0:
+            selection.append(float(row['Sales Ratio']))
+df = pd.DataFrame(selection)
+df.boxplot(showfliers=False)
+plt.ylabel('Sales Ratio')
+plt.xlabel(' ')
+plt.show()
