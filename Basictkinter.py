1e303c156fa2ba32f4c2fcbefbea82434a559299
Stivers-A <149453330+Stivers-A@users.noreply.github.com>
2024-11-26 21:30:45 -0800

    I now know have set up text boxes

--- Basictkinter.py
+++ Basictkinter.py
@@ -1,9 +1,21 @@
 
 import tkinter as tk
 
-r = tk.Tk()
-r.title('Press a button!')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+window = tk.Tk()
+
+window.title('Press a button!')
+#the gui is titled Press a button!
+greeting = tk.Label(text="Hello, Tkinter")
+#creates a label widget with the text Hello, Tkinter assigns it to the vairable greeting
+greeting.pack()
+#adds the greeting widget to the gui 
+
+button = tk.Button(window, text='Stop', width=25, command=window.destroy)
 # command r.destory ends program
 button.pack()
-r.mainloop()
+
+
+window.mainloop()
+# mainloop tells python to run the tk main loop, basically stuff after this wont run until the window is closed
+print('Hello World')
+#this displays in the terminal after the gui is closed
